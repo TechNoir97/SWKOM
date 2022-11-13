abfbe38c4c34c74926d5cbf9986619a6ebda81c3
TechNoir <56191589+TechNoir97@users.noreply.github.com>
2022-11-13 12:44:33 +0100

    Unit tests überarbeitung
    Unit tests werden neu überarbeitet für repositories

--- src/main/java/at/fhtw/swen3/persistence/repositories/ParcelRepository.java
+++ src/main/java/at/fhtw/swen3/persistence/repositories/ParcelRepository.java
@@ -5,8 +5,8 @@ import at.fhtw.swen3.persistence.entity.RecipientEntity;
 import io.swagger.models.auth.In;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ParcelRepository extends JpaRepository<ParcelEntity, Integer> {
-    ParcelEntity findById(int id);
+public interface ParcelRepository extends JpaRepository<ParcelEntity, Long> {
+    ParcelEntity findById(long id);
     ParcelEntity findByTrackingId(String trackingId);
     ParcelEntity findBySender(RecipientEntity sender);
     ParcelEntity findByRecipient(RecipientEntity recipient);
---
+++ src/test/java/at/fhtw/swen3/services/impl/ParcelApiControllerTest.java
@@ -0,0 +1,34 @@
+package at.fhtw.swen3.services.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ParcelApiControllerTest {
+
+    @Autowired
+    private final ParcelApiController parcelApiController;
+
+    @Test
+    void reportParcelDelivery() {
+    }
+
+    @Test
+    void reportParcelHop() {
+    }
+
+    @Test
+    void submitParcel() {
+    }
+
+    @Test
+    void trackParcel() {
+    }
+
+    @Test
+    void transitionParcel() {
+    }
+}
