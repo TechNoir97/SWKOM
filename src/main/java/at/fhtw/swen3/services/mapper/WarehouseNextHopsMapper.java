5622ab05088f6e1c7e85e95419eb83c0841e22d6
TechNoir <56191589+TechNoir97@users.noreply.github.com>
2022-11-27 12:22:43 +0100

    oi

--- src/main/java/at/fhtw/swen3/services/mapper/WarehouseNextHopsMapper.java
+++ src/main/java/at/fhtw/swen3/services/mapper/WarehouseNextHopsMapper.java
@@ -2,8 +2,9 @@ package at.fhtw.swen3.services.mapper;
 
 import at.fhtw.swen3.persistence.entities.WarehouseNextHopsEntity;
 import at.fhtw.swen3.services.dto.WarehouseNextHops;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
-
+@Mapper
 public interface WarehouseNextHopsMapper {
     WarehouseNextHopsMapper INSTANCE = Mappers.getMapper(WarehouseNextHopsMapper.class);
     WarehouseNextHops entityToDto(WarehouseNextHopsEntity warehouseNextHopsEntity);
